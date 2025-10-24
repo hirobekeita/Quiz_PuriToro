fb9ea4bdf8e84bc11cdc27d65f7bbb6c92c834bc
Keita Hirobe <144866386+hirobekeita@users.noreply.github.com>
2025-10-24 16:09:51 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Quiz_PuriToro/'
+  base: '/Quiz_PuriToro/',
+  build: {
+    outDir: 'dist',
+  },
 })
